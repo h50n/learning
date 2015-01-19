27af785fc6fa3f82e33d09ea81fa6d2e9aee8ccb
h50n <Chris@Chriss-MacBook-Pro-2.local>
2015-01-19 01:39:10 +0100

    New
    Test

---
+++ index.php
@@ -0,0 +1,18 @@
+
+
+<?php
+
+	"This is a change I am making";
+
+
+
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/xampp/');
+	exit;
+?>
+Something is wrong with the XAMPP installation :-(
